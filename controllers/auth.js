de9c33e69d820c8f2244ae899f364bfb97aedd1d
= <=>
2018-10-09 13:41:12 +0700

    auth change & workspace

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,9 +12,5 @@ module.exports.login = function (req, res) {
 
 module.exports.register = function (req, res) {
     // email password
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    })
-    user.save().then(() => console.log('User created'))
+
 }
